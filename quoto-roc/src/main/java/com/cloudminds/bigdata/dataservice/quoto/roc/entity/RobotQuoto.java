e799ccb6009f95a94c4a9e0afe26f01524934a68
qiong.tan <qiong.tan@cloudminds.com>
2021-01-05 13:34:22 +0800

    增加通用服务(目前是apijson的源码)

---
+++ quoto-roc/src/main/java/com/cloudminds/bigdata/dataservice/quoto/roc/entity/RobotQuoto.java
@@ -0,0 +1,25 @@
+package com.cloudminds.bigdata.dataservice.quoto.roc.entity;
+
+import lombok.Data;
+
+//@Data
+//public class RobotQuoto {
+//	private String tenantId;
+//	private String rebotId;
+//	private String robotType;
+//	private String userId;
+//	private String rodType;
+//	private String time;
+//	private float robotOnlineTick;
+//	private int robotConnect;
+//	private int robotAbnormalDisconnect;
+//	private int robotAlarm;
+//	private int FRCounter;
+//	private int voiceClipCounter;
+//	private int asrCounter;
+//	private long avStreamSize;
+//	private long avStreamDuration;
+//	private int hiInventionCounter;
+//	private int aiResponseCounter;
+//
+//}
