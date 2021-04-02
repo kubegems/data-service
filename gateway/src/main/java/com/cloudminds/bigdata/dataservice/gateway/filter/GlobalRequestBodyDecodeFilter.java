c6518eb5bb46004a09740718c7a46fcb852c9929
qiong.tan <qiong.tan@cloudminds.com>
2021-04-02 15:58:01 +0800

    打印远端地址

---
+++ gateway/src/main/java/com/cloudminds/bigdata/dataservice/gateway/filter/GlobalRequestBodyDecodeFilter.java
@@ -0,0 +1,31 @@
+package com.cloudminds.bigdata.dataservice.gateway.filter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 对所有的request请求的body进行解密
+ * @date 2020/12/4 17:34
+ * @author wei.heng
+ */
+@Component
+public class GlobalRequestBodyDecodeFilter implements GlobalFilter, Ordered {
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		ServerHttpRequest request = exchange.getRequest();
+		System.out.println(request.getRemoteAddress().getAddress());
+		return chain.filter(exchange);
+	}
+
+	@Override
+	public int getOrder() {
+		return Integer.MIN_VALUE;
+	}
+
+
+}
