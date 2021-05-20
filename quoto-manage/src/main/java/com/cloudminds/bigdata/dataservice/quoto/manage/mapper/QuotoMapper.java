92f7144fc3a0eef3cc48eaaf8c43139f9d6e93a6
qiong.tan <qiong.tan@cloudminds.com>
2021-05-20 16:48:57 +0800

    优化代码

--- quoto-manage/src/main/java/com/cloudminds/bigdata/dataservice/quoto/manage/mapper/QuotoMapper.java
+++ quoto-manage/src/main/java/com/cloudminds/bigdata/dataservice/quoto/manage/mapper/QuotoMapper.java
@@ -82,7 +82,7 @@ public interface QuotoMapper {
 	public int insertQuoto(Quoto quoto);
 
 	@Update("update quoto set name=#{name}, field=#{field},business_process_id=#{business_process_id},quoto_level=#{quoto_level},data_type=#{data_type},data_unit=#{data_unit},table_id=#{table_id},accumulation=#{accumulation},origin_quoto=#{origin_quoto},cycle=#{cycle},"
-			+ "dimension=#{dimension,typeHandler=com.cloudminds.bigdata.dataservice.quoto.manage.entity.handler.ArrayTypeHandler},adjective=#{adjective,typeHandler=com.cloudminds.bigdata.dataservice.quoto.manage.entity.handler.ArrayTypeHandler},,quotos=#{quotos,typeHandler=com.cloudminds.bigdata.dataservice.quoto.manage.entity.handler.ArrayTypeHandler},type=#{type},descr=#{descr} where id=#{id}")
+			+ "dimension=#{dimension,typeHandler=com.cloudminds.bigdata.dataservice.quoto.manage.entity.handler.ArrayTypeHandler},adjective=#{adjective,typeHandler=com.cloudminds.bigdata.dataservice.quoto.manage.entity.handler.ArrayTypeHandler},quotos=#{quotos,typeHandler=com.cloudminds.bigdata.dataservice.quoto.manage.entity.handler.ArrayTypeHandler},type=#{type},descr=#{descr} where id=#{id}")
 	public int updateQuoto(Quoto quoto);
 
 	@Select("select * from cycle")
