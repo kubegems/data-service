5cdf8c9cb06429064f437c5a55ef871cc80240d1
qiong.tan <qiong.tan@cloudminds.com>
2021-05-24 15:31:36 +0800

    更改包路径

--- quoto-manage/src/main/java/com/cloudminds/bigdata/dataservice/quoto/manage/service/QuotoService.java
+++ quoto-manage/src/main/java/com/cloudminds/bigdata/dataservice/quoto/manage/service/QuotoService.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.Stack;
 
-import org.apache.commons.collections.SetUtils;
+import org.apache.commons.collections4.SetUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
