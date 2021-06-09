9e3a1d7cf9c80a9d383088a701d87ccc33760be2
qiong.tan <qiong.tan@cloudminds.com>
2021-06-09 15:44:14 +0800

    优化代码

--- quoto-manage/src/main/java/com/cloudminds/bigdata/dataservice/quoto/manage/controller/QuotoControl.java
+++ quoto-manage/src/main/java/com/cloudminds/bigdata/dataservice/quoto/manage/controller/QuotoControl.java
@@ -1,13 +1,9 @@
 package com.cloudminds.bigdata.dataservice.quoto.manage.controller;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.nacos.api.utils.StringUtils;
