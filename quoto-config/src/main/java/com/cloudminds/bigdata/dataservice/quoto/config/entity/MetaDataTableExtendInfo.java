4def96844d224ca74560d402fa3dbcb33e22e424
qiong.tan <qiong.tan@cloudminds.com>
2022-12-15 15:33:57 +0800

    元数据管理接口开发

--- quoto-config/src/main/java/com/cloudminds/bigdata/dataservice/quoto/config/entity/MetaDataTableExtendInfo.java
+++ quoto-config/src/main/java/com/cloudminds/bigdata/dataservice/quoto/config/entity/MetaDataTableExtendInfo.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class MetaDataTableExtendInfo extends MetaDataTable{
-    private String business_process_name;
     private String theme_name;
     private int business_id_three_level;
     private String business_name_three_level;
