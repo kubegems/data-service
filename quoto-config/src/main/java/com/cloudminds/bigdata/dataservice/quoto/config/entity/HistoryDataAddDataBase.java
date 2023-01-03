4f28d5779c1e47d5d83b1f6d3be69fd17a859939
qiong.tan <qiong.tan@cloudminds.com>
2023-01-03 17:10:54 +0800

    元数据管理更新接口更改&录入历史数据接口开发

---
+++ quoto-config/src/main/java/com/cloudminds/bigdata/dataservice/quoto/config/entity/HistoryDataAddDataBase.java
@@ -0,0 +1,13 @@
+package com.cloudminds.bigdata.dataservice.quoto.config.entity;
+
+import lombok.Data;
+
+@Data
+public class HistoryDataAddDataBase {
+    private String database_name;
+    private String table_name;
+    private int table_type;
+    private String model_level;
+    private int theme_id;
+    private String data_domain;
+}
